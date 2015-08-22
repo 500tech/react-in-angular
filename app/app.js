acad524153fe3c7da98e56402b0c35be2743d804
Boris Dinkevich <boris@dinkevich.com>
2015-08-22 13:59:51 +0300

    Add simple react render directive

--- app/app.js
+++ app/app.js
@@ -2,23 +2,33 @@
 
 import React, { Component } from 'react';
 
-angular.module('app', [])
-  .directive('pic', () => {
-    return {
-      template: `
-        <div>
-          <h2>AngularJS is here!</h2>
-          <div class="inside">Inside</div>
-        </div>
-      `
-    };
-  });
-
 class Pic extends Component {
   render() {
     return (<h2>ReactJS is here!</h2>);
   }
 }
+
+angular.module('app', [])
+  .directive('reactRender', () => {
+    return {
+      template: '<div></div>',
+      link: (scope, element, attrs) => {
+        const inside = element[0];
+        React.render(<Pic />, inside);
+      }
+    }
+  })
+
+  .directive('pic', () => {
+    return {
+      template: `
+        <div>
+          <h2>AngularJS is here!</h2>
+          <div class="inside" react-render>Inside</div>
+        </div>
+      `
+    };
+  });
 
 const app1 = document.getElementById('app1');
 const app2 = document.getElementById('app2');
@@ -27,5 +37,4 @@ const app3 = document.getElementById('app3');
 angular.bootstrap(app1, ['app']);
 angular.bootstrap(app2, ['app']);
 
-const inside = document.getElementsByClassName('inside')[0];
-React.render(<Pic />, inside);
+
